e9e9e44c37600e3cf30d246a3a0a441c5d891152
DragonGuyMan <108588518+DragonGuyMan@users.noreply.github.com>
2024-07-30 00:22:41 -0400

    Initial commit

---
+++ src/main/java/ajs/dragonguyman/playlistextract/FileManipulator.java
@@ -0,0 +1,54 @@
+package ajs.dragonguyman.playlistextract;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Contains methods used for file operations.
+ * 
+ * @author austin
+ *
+ */
+public class FileManipulator {
+	
+	/**
+	 * Creates a new file based off given path name
+	 * 
+	 * @param pathname Name of file and its path. Shouldn't be a null value.
+	 * @throws IOException If I/O failure occurs.
+	 */
+	public static void createFile(String pathname) throws IOException {
+		
+		File file = new File(pathname);
+		
+		if (file.createNewFile()) {
+			System.out.println("File created at " + file.getAbsolutePath());
+		}
+		else {
+			System.out.println("This file already exists.");
+		}
+	}
+	
+	/**
+	 * Writes strings in an array list to a file. Each string is put on its own line.
+	 * 
+	 * @param stringList List of strings to put on seperate lines
+	 * @param pathname Path of file to write to
+	 * @throws IOException For problems with I/O
+	 */
+	public static void onePerLine (ArrayList<String> stringList, String pathname) throws 
+			IOException {
+		
+		FileWriter file = new FileWriter(pathname);
+		BufferedWriter output = new BufferedWriter(file);
+		
+		for (String text : stringList) {
+			output.write(text + "\n");
+		}
+		
+		output.close();
+	}
+}
---
+++ src/main/java/ajs/dragonguyman/playlistextract/PlaylistExtractor.java
@@ -0,0 +1,7 @@
+package ajs.dragonguyman;
+
+public class PlaylistExtractor {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
